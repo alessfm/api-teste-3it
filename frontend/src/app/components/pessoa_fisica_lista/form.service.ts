517050abc801625f37157c026caa7d0919e87e0a
alessfm <alessandrofm@aluno.fapce.edu.br>
2021-10-20 16:57:51 -0300

    service with getPeople

---
+++ frontend/src/app/components/pessoa_fisica_lista/form.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pessoas } from './pessoa-fisica.module'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormService {
+
+  constructor(private http: HttpClient){}
+
+  getPeople():Observable<Pessoas[]>{
+    return this.http.get<Pessoas[]>('http://localhost:8080/pessoas')
+  }
+}
