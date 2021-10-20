3e650ff1139b8fd6d74aa8e8c6c9c39862d5cc2f
alessfm <alessandrofm@aluno.fapce.edu.br>
2021-10-20 16:57:35 -0300

    main app

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PessoaFisicaComponent } from './components/pessoa_fisica/pessoa-fisica.component';
+import { PessoaFisicaComponent } from './components/pessoa_fisica_lista/pessoa-fisica.component';
 
 const routes: Routes = [
   { path: 'pessoas', component: PessoaFisicaComponent }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { PessoaFisicaComponent } from './components/pessoa_fisica/pessoa-fisica.component';
-import { FormService } from './components/pessoa_fisica/form.service';
+
+import { PessoaFisicaComponent } from './components/pessoa_fisica_lista/pessoa-fisica.component';
+import { FormService } from './components/pessoa_fisica_lista/form.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PessoaFisicaComponent
+    PessoaFisicaComponent,
   ],
   imports: [
     BrowserModule,
