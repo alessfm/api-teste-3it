fbb6e5bdcaccc924af9a5e6cbb9ad429b7237332
alessfm <alessandrofm@aluno.fapce.edu.br>
2021-10-21 12:32:56 -0300

    fix: CORS blocked routes

--- api/src/main/java/br/com/api/crud/controller/EstadoController.java
+++ api/src/main/java/br/com/api/crud/controller/EstadoController.java
@@ -3,6 +3,7 @@ package br.com.api.crud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.crud.model.Estado;
 import br.com.api.crud.repository.EstadoRepository;
+
+@CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
 @RequestMapping("/estados")
--- api/src/main/java/br/com/api/crud/controller/PessoaFisicaController.java
+++ api/src/main/java/br/com/api/crud/controller/PessoaFisicaController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,8 @@ import br.com.api.crud.controller.dto.EstadoDto;
 import br.com.api.crud.controller.dto.PessoaFisicaDto;
 import br.com.api.crud.model.PessoaFisica;
 import br.com.api.crud.service.PessoaFisicaService;
+
+@CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
 @RequestMapping("/pessoas")
