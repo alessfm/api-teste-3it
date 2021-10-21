aad4d502591a64b55a5dfdc4e5ed4a2981407634
alessfm <alessandrofm@aluno.fapce.edu.br>
2021-10-21 00:10:22 -0300

    enchacement: Service and component for Estados e Cidades

--- frontend/src/app/components/pessoa_fisica_lista/form.service.ts
+++ frontend/src/app/components/pessoa_fisica_lista/form.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Pessoas } from '../pessoa-fisica.module'
+import { Cidades } from '../cidade.module'
+import { Estados } from '../estado.module'
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,13 @@ export class FormService {
 
   getPeople():Observable<Pessoas[]>{
     return this.http.get<Pessoas[]>('http://localhost:8080/pessoas')
+  }
+
+  getCities():Observable<Cidades[]>{
+    return this.http.get<Cidades[]>('http://localhost:8080/municipios')
+  }
+
+  getStates():Observable<Estados[]>{
+    return this.http.get<Estados[]>('http://localhost:8080/estados')
   }
 }
--- frontend/src/app/components/pessoa_fisica_lista/pessoa-fisica.component.ts
+++ frontend/src/app/components/pessoa_fisica_lista/pessoa-fisica.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormService } from './form.service';
 import { Pessoas } from '../pessoa-fisica.module'
+import { Cidades } from '../cidade.module'
+import { Estados } from '../estado.module'
 
 @Component({
   selector: 'app-pessoa-fisica',
@@ -8,12 +10,21 @@ import { Pessoas } from '../pessoa-fisica.module'
 })
 export class PessoaFisicaComponent implements OnInit {
   pessoas: Pessoas[]
+  cidades: Cidades[]
+  estados: Estados[]
   
   constructor(private getAllPeopleService: FormService) { }
 
   ngOnInit() {
     this.getAllPeopleService.getPeople().subscribe(pessoas =>{
       this.pessoas = pessoas
+    })
+    this.getAllPeopleService.getCities().subscribe(cidades =>{
+      this.cidades = cidades
+      console.log(this.cidades)
+    })
+    this.getAllPeopleService.getStates().subscribe(estados =>{
+      this.estados = estados
     })
   } 
 
